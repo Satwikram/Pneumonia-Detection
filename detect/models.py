cbe10a8c20f50f2c84e47936836eb7cf4b508016
Satwik Ram <satwikram29@gmail.com>
2020-08-03 12:44:12 +0530

    Updated for deployment

--- detect/models.py
+++ detect/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class PneumoniaPredict:
+class PneumoniaPredict(models.Model):
     img_path = models.CharField(max_length = 20)
-    predict = models.CharField(max_length = 20)
+    prediction = models.CharField(max_length = 20)
--- detect/views.py
+++ detect/views.py
@@ -1,10 +1,15 @@
+from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
+import os
 import tensorflow as tf
 from tensorflow import keras
 import cv2
 
 # Create your views here.
+from detect.models import PneumoniaPredict
+
+
 def predict(request):
     if request.method == 'POST':
         try:
@@ -14,9 +19,41 @@ def predict(request):
 
             filename = str(image.name)
             fs = FileSystemStorage(location = folder)
+            name = fs.save(image.name, image)
+
+            mediapath = folder + "{}"
+            filepath = os.path.join(mediapath).format(name)
+
+            # Loading the model
+            model = tf.keras.models.load_model('models\pneumonia.h5')
+
+            img_size = 300
+            img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+            new_array = cv2.resize(img_array, (img_size, img_size))
+            new_array.reshape(-1, img_size, img_size, 1)
+
+            Labels = ['pneumonia', 'normal']
+
+            prediction = model.predict([new_array])
+
+            result = Labels[int(prediction[0][0])]
+            print(result)
+
+            pn = PneumoniaPredict()
+
+            pn.img_path = filepath
+            pn.prediction = result
+            pn.save()
+
+            if result == 'pneumonia':
+                messages.info(request, "The patient condition is: Pneumonia")
+            else:
+                messages.info(request,"The patient condition is: Normal")
+
+            return render(request, 'results.html')
+
         except Exception as e:
             print("The Error is:",e)
-
 
     else:
         return render(request, 'index.html')
